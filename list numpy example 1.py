3e8b2d365665d3792e00a94b642bda03b0abfd6b
kishan9999 <ksnjsi@gmail.com>
2022-02-01 19:55:29 +0530

    Add files via upload

---
+++ list numpy example 1.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+#list
+a1 = [1,2,3,4,5]
+b1 = [6,7,8,9,10]
+
+print(a1+b1)
+
+print(a1*2)
+
+# print(a1*b1) #error
+
+#numpy
+a2 = np.array(a1)
+b2 = np.array(b1)
+
+print(a2+b2)
+
+print(a2*2)
+
+print(a2*b2)
